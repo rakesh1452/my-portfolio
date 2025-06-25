824ca38b31f07c6597531b372b26e581ce13b4d2
Rakesh Pal <rakesh.pal1452@gmail.com>
2025-06-25 12:28:22 +0530

    Added smooth scroll feature

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -19,17 +19,18 @@ return (
                      />
          </span>
       </Link>
-      <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
-         © 2020 Tailblocks —
-         <a
-            href="https://twitter.com/knyttneve"
-            className="text-gray-600 ml-1"
-            rel="noopener noreferrer"
-            target="_blank"
-            >
-         @knyttneve
-         </a>
-      </p>
+     <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
+  © {new Date().getFullYear()} Rakesh Pal —
+  <a
+    href="https://www.linkedin.com/in/rakesh-pal-478031146/"
+    className="text-gray-600 ml-1"
+    rel="noopener noreferrer"
+    target="_blank"
+  >
+    @rakeshpal
+  </a>
+</p>
+
     <div className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start space-x-2">
   {/* Facebook */}
   <Link
@@ -64,9 +65,6 @@ return (
     </div>
   </Link>
 </div>
-
-
-
 
    </div>
 </footer>
